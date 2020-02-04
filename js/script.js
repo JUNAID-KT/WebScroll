b5917092db6e60fefd40801f647bb880d5e94767
JUNAID-KT <ktjunaid22@gmail.com>
2020-02-04 20:15:22 +0530

    js added, web page defined

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function GetURL() {
+    /*
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert(this.responseText);
+         }
+    };
+    xhttp.open("GET", "https://github.com", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    */
+    document.getElementById("result").innerHTML = "Paragraph changed.";
+    alert("Haiiii");
+}
